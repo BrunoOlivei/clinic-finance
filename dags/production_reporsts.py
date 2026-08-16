784a1b98967ed3e8e5e99eb8bc7aa2746d1727be
Bruno Oliveira <brunoolivei@protonmail.com>
2026-08-16 10:46:18 +0000

    Dag para relatório de produção

---
+++ dags/production_reporsts.py
@@ -0,0 +1,31 @@
+from datetime import datetime, timedelta
+
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+
+with DAG(
+    dag_id="ProductionReports",
+    start_date=datetime(2026, 1, 1),
+    schedule=None,
+    catchup=False,
+) as dag:
+    task_extract_production = BashOperator(
+        task_id="extract_production",
+        execution_timeout=timedelta(minutes=30),
+        bash_command=(
+            "docker exec -w /workspace "
+            '$(docker ps -q --filter "label=com.docker.compose.service=app") '
+            "uv run python -m src.savi.production_reports"
+        ),
+    )
+
+    task_dbt_build = BashOperator(
+        task_id="dbt_build",
+        bash_command=(
+            "docker exec -w /workspace/dbt -e PYTHONPATH=. "
+            '$(docker ps -q --filter "label=com.docker.compose.service=app") '
+            "uv run dbt build --select stg_production_reports+"
+        ),
+    )
+
+    task_extract_production >> task_dbt_build
